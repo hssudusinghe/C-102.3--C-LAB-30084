60d119585a6e5b7197b4a807aaeb0f81c7f37746
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2023-06-19 04:30:50 -0700

    Add files via upload

---
+++ c lab 7.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+
+
+/*int main()
+{
+    char c;
+    int pi=3.14;
+    printf ("enter c for circumference:");
+    scanf("%c",&c);
+    float r;
+    printf("enter radious:");
+    scanf("%f",&r);
+    switch (c){
+    case'c':printf("circumference:%.2f\n",2*pi*r);break;
+    case'a': case'A':printf("area:%.2f\n",pi*r*r);break;
+    case'y':printf("volume:%.2f\n",(3/4)*pi*r*r*r);break;
+    default: printf("invalid choice\n");
+       }
+
+    return 0;*/
+
+
+  /*int main()
+
+    char ch;
+    printf("enter a lower case letter:");
+    scanf (" %c",&ch);
+    switch(ch){
+        case 'a' :
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u': printf("%c is a vowel",ch);break;
+        default: printf("consonant");
+        }
+        return 0;*/
+
+
+
+
+        /*int main ()
+        int m;
+        printf("enter the number of month:");
+        scanf("%d",&m);
+
+        switch (m)
+        case 2 :printf("28 darys"); break;
+        case 4 :case 6: case 9: case 11: printf ("30 days"); break;
+        default: printf("31 days");
+        return 0;*/
+
+
+
